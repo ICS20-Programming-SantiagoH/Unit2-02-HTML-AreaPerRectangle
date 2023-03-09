bf6eb064a3a3601cf0cfd31859d81d4cfe576306
santiagohewett <124379638+santiagohewett@users.noreply.github.com>
2023-03-09 16:06:00 +0000

    index final product bad linter

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 Santiago Hewett All rights reserved
+//
+// Created by: Santiago Hewett
+// Created on: Mar 2023
+// This file contains the JS functions for index.html
+
+function doPerimeterClicked () {
+  // this function does basic math
+  document.getElementById('add-math').innerHTML = 'What is the perimeter ' + (5 + 5 + 3 + 3)
+}
+
+function doAreaClicked () {
+  // this function does basic math
+  document.getElementById('multiply-math').innerHTML = 'What is the area ' + (5 * 3)
+}
